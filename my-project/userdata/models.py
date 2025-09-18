2b658327ae9e1ab8fe327bd806537e7ab9a78fce
rajputankit84574-max <rajputankit84574@gmail.com>
2025-09-18 18:20:33 +0530

    add-database

--- my-project/userdata/models.py
+++ my-project/userdata/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class user(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=40)
+    subject = models.CharField(max_length=50)
+    message = models.CharField(max_length=500)
